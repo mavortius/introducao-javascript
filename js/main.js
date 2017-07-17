e0b99eb113cf35958a25c2f48a8d0a1e5970df59
Marcelo Martins <marcelomartins@santos.sp.gov.br>
2017-07-17 18:14:42 -0300

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,27 +1,33 @@
 // main.js
 
-var pacient = document.querySelector("#first-pacient");
-var weightTD = pacient.querySelector(".info-weight");
-var weight = weightTD.textContent;
-var heightTD = pacient.querySelector(".info-height");
-var height = heightTD.textContent;
-var imcTD = pacient.querySelector(".info-imc");
-var isWeightValid = true;
-var isHeightValid = true;
+var patients = document.querySelectorAll(".patient");
 
-if(weight <= 0 || weight >= 300) {
-    console.log("Peso invalido!");
-    isWeightValid = false;
-    imcTD.textContent = "Peso invalido!";
-}
+for (var i = 0; i < patients.length; i++) {
+    var patient = patients[i];
+    var weightTD = patient.querySelector(".info-weight");
+    var weight = weightTD.textContent;
+    var heightTD = patient.querySelector(".info-height");
+    var height = heightTD.textContent;
+    var imcTD = patient.querySelector(".info-imc");
+    var isWeightValid = true;
+    var isHeightValid = true;
 
-if(height <= 0 || height >= 3.00) {
-    console.log("Altura invalida!");
-    isHeightValid = false;
-    imcTD.textContent = "Altura invalida!";
-}
+    if(weight <= 0 || weight >= 300) {
+        console.log("Peso invalido!");
+        isWeightValid = false;
+        imcTD.textContent = "Peso invalido!";
+        patient.classList.add("patient-invalid");
+    }
 
-if(isWeightValid && isHeightValid) {
-    var imc = weight / (height * height);
-    imcTD.textContent = imc;
+    if(height <= 0 || height >= 3.00) {
+        console.log("Altura invalida!");
+        isHeightValid = false;
+        imcTD.textContent = "Altura invalida!";
+        patient.classList.add("patient-invalid");
+    }
+
+    if(isWeightValid && isHeightValid) {
+        var imc = weight / (height * height);
+        imcTD.textContent = imc.toFixed(2);
+    }
 }
