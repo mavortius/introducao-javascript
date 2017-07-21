196cb213a125c863271a5f83fedc58c72e3631d4
Marcelo Martins <marcelomartins@santos.sp.gov.br>
2017-07-21 13:59:20 -0300

    Finalizaçao do curso

--- js/patients-search.js
+++ js/patients-search.js
@@ -6,13 +6,25 @@ addButton.addEventListener("click", function () {
     var request = new XMLHttpRequest();
 
     request.open("GET", "http://localhost:8080/api/patients");
-    request.addEventListener("load", function () {
-        var response = request.responseText;
-        var patients = JSON.parse(response);
 
-        patients.forEach(function (patient) {
-            addPatientsToTable(patient);
-        });
+    console.log(request.status);
+
+    request.addEventListener("load", function () {
+        var ajaxError = document.querySelector("#ajax-error");
+
+        console.log(request.status);
+
+        if(request.status === 200) {
+            var response = request.responseText;
+            var patients = JSON.parse(response);
+
+            ajaxError.classList.add("invisible");
+            patients.forEach(function (patient) {
+                addPatientsToTable(patient);
+            });
+        } else {
+            ajaxError.classList.remove("invisible");
+        }
     });
     request.send();
 });
